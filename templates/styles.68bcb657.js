5b70d4e63e7124bd621b8a41e6121419e6bc8acc
mromoli <marcoromoli95@gmail.com>
2020-01-28 17:19:17 +0100

    minor fixes on the images

--- templates/styles.68bcb657.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{140:function(n,w,o){}}]);
-//# sourceMappingURL=styles.68bcb657.js.map
---
+++ templates/styles.7c170f5f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{139:function(n,w,o){}}]);
+//# sourceMappingURL=styles.7c170f5f.js.map
